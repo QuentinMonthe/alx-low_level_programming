4eeac00ea0bfe2bf3a448aa6dcf9ad33eadae389
QuentinMonthe <quentinnash99@gmail.com>
2023-06-16 16:48:34 -0700

    Print text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,12 @@
 /**
  * The program print text with using the function puts
  *
+ * main - Entry point 
+ *
  * Return: Alwals 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
