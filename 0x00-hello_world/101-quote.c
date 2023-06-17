6d1c2db4b250de872080c8d58d8ead552e0be5d4
QuentinMonthe <quentinnash99@gmail.com>
2023-06-17 15:43:30 -0700

    Print text without printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,10 @@
 
 /**
  * main - This program print text with not using the function printf or puts.
- * Return: Always 0 (Success).
+ * Return: Always 1 (Success).
  */
 int main(void)
 {
-
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
