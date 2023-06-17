598904a24043384fd40dba8d86449a6fff333764
QuentinMonthe <quentinnash99@gmail.com>
2023-06-16 17:27:58 -0700

    Print text with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/*
+ * File: 5-printf.c
+ * Auth: Qunetin Monthe
+ */
+
+#include <stdio.h>
+
+/**
+ * main - This program print text with using the function printf.
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
