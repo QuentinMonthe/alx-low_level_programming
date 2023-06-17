157b86e5ac17751539953ca10abf5ce94bfecdad
QuentinMonthe <quentinnash99@gmail.com>
2023-06-16 17:37:51 -0700

    Size of various type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+/*
+ * File: 6-size.c
+ * Auth: Qunetin Monthe
+ */
+
+#include <stdio.h>
+
+/**
+ * main - This program print the size of parious type on this computer.
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", (int)sizeof(char));
+	printf("Size of an int: %d byte(s)\n", (int)sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", (int)sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", (int)sizeof(float));
+
+	return (0);
+}
