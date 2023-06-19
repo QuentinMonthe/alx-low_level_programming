17ac66298789d474b2bd3e870cfe8d935ccdeeda
QuentinMonthe <quentinnash99@gmail.com>
2023-06-19 03:12:32 -0700

    Checker wrong update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 
 /*
+ * betty style doc for function main goes there
  * main: This program print if the number is postive, zero, or negative.
  * Description: using the main function for return the sign of a random number
  * Return: Always 0 (Success).
@@ -18,6 +19,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
 	if (n == 0)
 	{
 		printf("%d is zero\n", n);
