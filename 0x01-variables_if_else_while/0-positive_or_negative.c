64e798d3792fafd94731655fe068ecb02ba84a50
QuentinMonthe <quentinnash99@gmail.com>
2023-06-19 01:44:53 -0700

    Sign of the number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,8 +11,6 @@
  * main - This program generate a random number and print it sign.
  * Return: Always 0 (Success).
  */
-
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
