d97494bb597708f3603e3700155b5b43205490ac
QuentinMonthe <quentinnash99@gmail.com>
2023-06-20 04:55:19 -0700

    Project functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry part of the program
+ *
+ * Description: This program print the _putchar
+ *
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the README.md directory for the third project of learning C programming
