7d56f0213cf8b49d9d17fc06ffd3036c4f7f5d08
QuentinMonthe <quentinnash99@gmail.com>
2023-06-26 21:33:29 +0000

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - This function update pointer value
+ * @n: pointer to an intege
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	int value = 98;
+
+	*n = value;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is the README.md directory for the project 0x03-pointers_arrqys_strings of learning C programming
