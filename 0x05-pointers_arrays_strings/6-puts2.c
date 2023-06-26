c94b340a174c3cc092fda60e8d73893dbe07e171
QuentinMonthe <quentinnash99@gmail.com>
2023-06-26 23:30:37 +0000

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * _puts - This function prints a string.
+ * puts2 - This function prints a string.
  * @str: pointer to a char list (string)
  *
  * Return: Nothing
@@ -13,7 +13,12 @@ void puts2(char *str)
 	while (*str != '\0')
 	{
 		_putchar(*str);
-		str += 2;
+
+		if (*(str + 1) == '\0')
+		{
+			str++;
+		} else
+			str += 2;
 	}
 	_putchar('\n');
 }
