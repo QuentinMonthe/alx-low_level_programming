c57302c4f3b09d082967e40dd30e8171b439fa68
QuentinMonthe <quentinnash99@gmail.com>
2023-09-01 21:40:18 +0000

    Capitalize letter

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * cap_string - This function capitalizes all words of a string
+ * @str: string to change case
+ *
+ * Return: String in uppercase
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == 32 || str[i] == '\t' || str[i] == '\n'
+				|| str[i] == ',' || str[i] == ';' || str[i] == '.'
+				|| str[i] == '!' || str[i] == '?' || str[i] == 34
+				|| str[i] == '(' || str[i] == ')' || str[i] == '{'
+				|| str[i] == '}')
+		{
+			if (str[i + 1] > 96 && str[i + 1] < 123)
+			{
+				str[i + 1] = str[i + 1] - 32;
+				i = i + 2;
+			} else
+				i++;
+		} else
+			i++;
+	}
+
+	return (str);
+}
