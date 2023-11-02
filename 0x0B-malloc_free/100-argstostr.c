2a8daa29a252fe0abade082413752967e17bac07
QuentinMonthe <quentinnash99@gmail.com>
2023-11-02 12:50:45 +0000

    Concatenates all the arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -39,7 +39,7 @@ char *argstostr(int ac, char **av)
 	{
 		size += str_length(av[i]);
 	}
-	str = (char *) malloc(sizeof(char) * (size + ac));
+	str = (char *) malloc(sizeof(char) * (size + ac + 1));
 
 	if (str == NULL)
 	{
