bb0de8685b5306b931246f784d8c2acedefaac8a
QuentinMonthe <quentinnash99@gmail.com>
2023-11-08 11:43:32 +0000

    Initialize struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,49 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _str_cpy - this function copy the string
+ * @str1: string location copy
+ * @str2: string to copy
+ *
+ * Return: string (Success), NULL if not.
+ */
+char *_str_cpy(char *str1, char *str2)
+{
+	int i = 0;
+
+	if (str2 == NULL)
+		return (NULL);
+
+	str1 = malloc(sizeof(str2));
+
+	if (str1 == NULL)
+		return (NULL);
+
+	while(*(str2 + i) != '\0')
+	{
+		*(str1 + i) = *(str2 + i);
+			i++;
+	}
+	*(str1 + i) = '\0';
+
+	return (str1);
+}
+
+
+/**
+ * init_dog - this function initialize a variable dog
+ * @d: variable of dog type to initialize
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: Nothing.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = _str_cpy(d->name, name);
+	d->owner = _str_cpy(d->owner, owner);
+	d->age = age;
+}
