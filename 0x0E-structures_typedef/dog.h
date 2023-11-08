215e5140432e2e3978c701ff1bc1ab23e9eeb972
QuentinMonthe <quentinnash99@gmail.com>
2023-11-08 10:52:52 +0000

    Define a struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - define a type dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * This struct define the proporties of a dog in program
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* DOG_H */
